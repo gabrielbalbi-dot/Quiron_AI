17c09cccfcf51921446a211c435763ad339e33e1
gabrielbalbi-dot <gabriel.balbi@colegiologosofico.edu.ar>
2025-08-28 10:28:32 -0300

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import PercyJacksonAI from './components/PercyJacksonAI';
+
+function App() {
+  return (
+    <div className="App">
+      <PercyJacksonAI />
+    </div>
+  );
+}
+
+export default App;
